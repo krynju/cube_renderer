1d0a7bc92c3b8b67606e195bbf96439f2eec5856
Krystian Gulinski <gulinski.krystian@gmail.com>
2018-11-11 12:26:05 +0100

    refactor iteration registers and added more comments

--- mips/main.asm
+++ mips/main.asm
@@ -1,13 +1,3 @@
-.eqv	cube_rotation_matrix_r0c0 cube_rotation_matrix
-.eqv	cube_rotation_matrix_r0c1 cube_rotation_matrix + 4
-.eqv	cube_rotation_matrix_r0c2 cube_rotation_matrix + 8
-.eqv	cube_rotation_matrix_r1c0 cube_rotation_matrix + 12
-.eqv	cube_rotation_matrix_r1c1 cube_rotation_matrix + 16
-.eqv	cube_rotation_matrix_r1c2 cube_rotation_matrix + 20
-.eqv	cube_rotation_matrix_r2c0 cube_rotation_matrix + 24
-.eqv	cube_rotation_matrix_r2c1 cube_rotation_matrix + 28
-.eqv	cube_rotation_matrix_r2c2 cube_rotation_matrix + 32
-
 .eqv	CUBE_ROT_MATRIX_SIZE	36
 .eqv	VERTICES_SIZE		24
 .eqv	VECTOR_SIZE		12
@@ -17,58 +7,64 @@
 	cube_rotation_matrix:	.float	1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
 	## cube position vector 3x1 -> 3 floats -> 12 bytes
 	cube_position_vector:	.float	1.0, 2.0, 3.0
-	
+	## cube vertices array of vectors, each vector 4 bytes -> 2x3x4 -> 24 bytes 
+	## TEMPORARLY ONLY 2 VERTICES FOR TESTING PURPOSES	
 	vertices:		.float	1.0, 1.0, 1.0, -1.0, -1.0, -1.0
-	
 .text
-	li	$t0, 36					## iterate over matrix rows
-	li	$t1, 12					## iterate over vector rows
+main:
+	## REGISTERS USED IN ITERATION AND OFFSET
+	##	$t0 					## matrix row offset, start at sizeof(matrix)
+	##	$t1 					## position vector row offset, start at sizeof(pos_vector)
+	##	$t2					## currently calculated element offset	
+	##	$t3					## vertices iteration, starts at sizeof(vertices)
 	
-	li	$t3, 0					## current element calculated in the vertex vector
-	matrix_loop:
-	sub	$t0, $t0, 12
-	sub	$t1, $t1, 4
+	li	$t0, 36					## matrix row offset, load with sizeof(matrix)
+	li	$t1, 12					## position vector row offset, load with sizeof(pos_vector)
+matrix_loop:
+	sub	$t0, $t0, 12				## decrement matrix offset
+	sub	$t1, $t1, 4				## decrement pos_vector offset
 	
-	lwc1	$f2, cube_rotation_matrix($t0)
+	lwc1	$f2, cube_rotation_matrix($t0)		## load matrix row 
 	lwc1	$f3, cube_rotation_matrix+4($t0)
 	lwc1	$f4, cube_rotation_matrix+8($t0)
-	lwc1	$f5, cube_position_vector($t1)
+	lwc1	$f5, cube_position_vector($t1)		## load position vector element
 	
-	li	$t2, 24					## iterate over vertices (vertices size)
-	vertices_loop:
-	sub	$t2, $t2, 12				## decrement the vertices offset
-	add	$t3, $t2, $t1				## calculate the current element offset
+	li	$t3, 24					## vertices iteration, load with sizeof(vertices)
+vertices_loop:
+	sub	$t3, $t3, 12				## decrement the vertices offset
+	add	$t2, $t3, $t1				## calculate the current element offset
 	
-	lwc1	$f6, vertices($t2)
-	lwc1	$f7, vertices+4($t2)
-	lwc1	$f8, vertices+8($t2)
+	lwc1	$f6, vertices($t3)			## load vertex vector
+	lwc1	$f7, vertices+4($t3)
+	lwc1	$f8, vertices+8($t3)
 	
-	mov.s	$f0, $f5
-	mul.s	$f1, $f2, $f6
+	mov.s	$f0, $f5				## 0 + pos_vector * 1 
+	mul.s	$f1, $f2, $f6				## mxm1 * vertex_vector1
 	add.s	$f0, $f0, $f1
-	mul.s	$f1, $f3, $f7
+	mul.s	$f1, $f3, $f7				## mxm2 * vertex_vector2
 	add.s	$f0, $f0, $f1
-	mul.s	$f1, $f4, $f8
+	mul.s	$f1, $f4, $f8				## mxm3 * vertex_vector3
 	add.s	$f0, $f0, $f1
 	
-	swc1	$f0, vertices($t3)			## save the result
+	swc1	$f0, vertices($t2)			## save the result
 	
-	bnez	$t2, vertices_loop
-	bnez	$t0, matrix_loop
+	bnez	$t3, vertices_loop			## inner loopback - vertices iteration
+	bnez	$t0, matrix_loop			## outer loopback - matrix row iteration
 	
 
+	## temporary print of 2 vertices 
 	li	$t0, 24
 	li	$v0, 2
-	
-	print_loop:
+	li	$a0, '\n'
+print_loop:
 	sub	$t0, $t0, 4
 	lwc1	$f12, vertices($t0)
 	li	$v0, 2
-	syscall
-	li	$a0, '\n'
+	syscall						## print float
 	li	$v0, 11
-	syscall
+	syscall						## print newline
 	bnez	$t0, print_loop
 	
+exit:
 	li	$v0, 10
 	syscall
